06d3e3e12359ab39bf754f34ea8bc792033391a6
Anirudh Mishra <anirudh5mar@gmail.com>
2022-12-27 12:38:23 +0530

    Created service interface implementation of sgm

---
+++ pkg/social_graph_manager/social_graph_manager.go
@@ -0,0 +1,49 @@
+package social_graph_manager
+
+import (
+	"errors"
+	"log"
+
+	om "github.com/ani5msr/microservices-project/pkg/object_model"
+)
+
+type SocialGraphManager struct {
+	store om.SocialGraphManager
+}
+
+func NewSocialGraphManager(store om.SocialGraphManager) (om.SocialGraphManager, error) {
+	if store == nil {
+		return nil, errors.New("store can't be nil")
+	}
+	return &SocialGraphManager{store: store}, nil
+}
+
+func (m *SocialGraphManager) Follow(followed string, follower string) (err error) {
+	if followed == "" || follower == "" {
+		err = errors.New("followed and follower can't be empty")
+		return
+	}
+
+	return m.store.Follow(followed, follower)
+}
+
+func (m *SocialGraphManager) Unfollow(followed string, follower string) (err error) {
+	if followed == "" || follower == "" {
+		err = errors.New("followed and follower can't be empty")
+		return
+	}
+
+	return m.store.Unfollow(followed, follower)
+}
+
+func (m *SocialGraphManager) GetFollowing(username string) (map[string]bool, error) {
+	return m.store.GetFollowing(username)
+}
+
+func (m *SocialGraphManager) GetFollowers(username string) (result map[string]bool, err error) {
+	result, err = m.store.GetFollowers(username)
+	if err != nil {
+		log.Printf("LastError in GetFollowers(), %v\n", err)
+	}
+	return
+}
