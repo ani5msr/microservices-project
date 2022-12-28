9ce857eaa9493f90041e5d3bda824c8341e5b15f
Anirudh Mishra <anirudh5mar@gmail.com>
2022-12-28 21:02:22 +0530

    Created post manager and implemented func

--- pkg/object_model/interfaces.go
+++ pkg/object_model/interfaces.go
@@ -1,10 +1,10 @@
 package object_model
 
-type ThoughtManager interface {
-	GetThought(request GetLinksRequest) (GetLinksResult, error)
-	AddThought(request AddLinkRequest) error
-	UpdateThought(request UpdateLinkRequest) error
-	DeleteThought(username string, url string) error
+type PostManager interface {
+	GetPost(request GetPostsRequest) (GetPostsResult, error)
+	AddPost(request AddPostRequest) error
+	UpdatePost(request UpdatePostRequest) error
+	DeletePost(username string, url string) error
 }
 
 type UserManager interface {
@@ -29,12 +29,12 @@ type NewsManager interface {
 	GetNews(request GetNewsRequest) (GetNewsResult, error)
 }
 
-type ThoughtManagerEvents interface {
-	OnThoughtAdded(username string, link *Link)
-	OnThoughtUpdated(username string, link *Link)
-	OnThoughtDeleted(username string, url string)
+type PostManagerEvents interface {
+	OnPostAdded(username string, post *Post)
+	OnPostUpdated(username string, post *Post)
+	OnPostDeleted(username string, url string)
 }
 
-type ThoughtCheckerEvents interface {
-	OnThoughtChecked(username string, url string, status LinkStatus)
+type PostCheckerEvents interface {
+	OnPostChecked(username string, url string, status PostStatus)
 }
---
+++ pkg/post_manager/post_manager.go
@@ -0,0 +1,200 @@
+package post_manager
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	om "github.com/ani5msr/microservices-project/pkg/object_model"
+)
+
+// Nuclio functions listen by default on port 8080 of their service IP
+const Post_checker_func_url = "http://Post-checker.nuclio.svc.cluster.local:8080"
+
+type PostManager struct {
+	postStore          PostStore
+	socialGraphManager om.SocialGraphManager
+	eventSink          om.PostManagerEvents
+	maxPostsPerUser    int64
+}
+
+func (m *PostManager) GetPost(request om.GetPostsRequest) (result om.GetPostsResult, err error) {
+	if request.Username == "" {
+		err = errors.New("user name can't be empty")
+		return
+	}
+
+	result, err = m.postStore.GetPost(request)
+	if result.Posts == nil {
+		result.Posts = []om.Post{}
+	}
+	return
+}
+
+// Very wasteful way to count Posts
+func (m *PostManager) getPostCount(username string) (PostCount int64, err error) {
+	req := om.GetPostsRequest{Username: username}
+	res, err := m.GetPost(req)
+	if err != nil {
+		return
+	}
+
+	PostCount += int64(len(res.Posts))
+
+	for res.NextPageToken != "" {
+		req = om.GetPostsRequest{Username: username, StartToken: res.NextPageToken}
+		res, err = m.GetPost(req)
+		if err != nil {
+			return
+		}
+
+		PostCount += int64(len(res.Posts))
+	}
+	return
+}
+
+func triggerPostCheck(username string, url string) {
+	go func() {
+		checkPostRequest := &om.CheckPostRequest{Username: username, Url: url}
+		data, err := json.Marshal(checkPostRequest)
+		if err != nil {
+			return
+		}
+
+		req, err := http.NewRequest("POST", Post_checker_func_url, bytes.NewBuffer(data))
+		req.Header.Set("Content-Type", "application/json")
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			return
+		}
+		defer resp.Body.Close()
+	}()
+}
+
+func (m *PostManager) AddPost(request om.AddPostRequest) (err error) {
+	if request.Url == "" {
+		return errors.New("the URL can't be empty")
+	}
+
+	if request.Username == "" {
+		return errors.New("the user name can't be empty")
+	}
+
+	PostCount, err := m.getPostCount(request.Username)
+	if err != nil {
+		return
+	}
+
+	if PostCount >= m.maxPostsPerUser {
+		return errors.New("the user has too many Posts")
+	}
+
+	Post, err := m.postStore.AddPost(request)
+	if err != nil {
+		return
+	}
+
+	if m.eventSink != nil {
+		followers, err := m.socialGraphManager.GetFollowers(request.Username)
+		if err != nil {
+			return err
+		}
+
+		for follower := range followers {
+			m.eventSink.OnPostAdded(follower, Post)
+		}
+	}
+
+	// Trigger Post check asynchronously (don't wait for result)
+	triggerPostCheck(request.Username, request.Url)
+	return
+}
+
+func (m *PostManager) UpdatePost(request om.UpdatePostRequest) (err error) {
+	if request.Url == "" {
+		return errors.New("the URL can't be empty")
+	}
+
+	if request.Username == "" {
+		return errors.New("the user name can't be empty")
+	}
+
+	Post, err := m.postStore.UpdatePost(request)
+	if err != nil {
+		return
+	}
+
+	if m.eventSink != nil {
+		followers, err := m.socialGraphManager.GetFollowers(request.Username)
+		if err != nil {
+			return err
+		}
+
+		for follower := range followers {
+			m.eventSink.OnPostUpdated(follower, Post)
+		}
+	}
+
+	return
+}
+
+func (m *PostManager) DeletePost(username string, url string) (err error) {
+	if url == "" {
+		return errors.New("the URL can't be empty")
+	}
+
+	if username == "" {
+		return errors.New("the user name can't be empty")
+	}
+
+	err = m.postStore.DeletePost(username, url)
+	if err != nil {
+		return
+	}
+
+	if m.eventSink != nil {
+		followers, err := m.socialGraphManager.GetFollowers(username)
+		if err != nil {
+			return err
+		}
+
+		for follower := range followers {
+			m.eventSink.OnPostDeleted(follower, url)
+		}
+	}
+	return
+}
+
+func (m *PostManager) OnPostChecked(username string, url string, status om.PostStatus) {
+	m.postStore.SetPostStatus(username, url, status)
+}
+
+func NewPostManager(PostStore PostStore,
+	socialGraphManager om.SocialGraphManager,
+	natsUrl string,
+	eventSink om.PostManagerEvents,
+	maxPostsPerUser int64) (om.PostManager, error) {
+	if PostStore == nil {
+		return nil, errors.New("Post store")
+	}
+
+	if eventSink != nil && socialGraphManager == nil {
+		return nil, errors.New("social graph manager can't be nil if event sink is not nil")
+	}
+
+	Post_manager := &PostManager{
+		postStore:          PostStore,
+		socialGraphManager: socialGraphManager,
+		eventSink:          eventSink,
+		maxPostsPerUser:    maxPostsPerUser,
+	}
+
+	// Subscribe PostManager to Post checker events if nats is ocnfigured
+	if natsUrl != "" {
+		Post_checker_events.Listen(natsUrl, Post_manager)
+	}
+
+	return Post_manager, nil
+}
