96cb9badddaaec573afc29896937c170a51ae180
Anirudh Mishra <anirudh5mar@gmail.com>
2022-12-31 22:12:31 +0530

    Created test suite for social graph

--- pkg/post_manager/post_manager_suite_test.go
+++ pkg/post_manager/post_manager_suite_test.go
@@ -1,12 +1,13 @@
 package post_manager
 
 import (
+	"testing"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"testing"
 )
 
-func TestLinkManager(t *testing.T) {
+func TestPostManager(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "LinkManager Suite")
 }
