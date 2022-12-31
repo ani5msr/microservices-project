a155817422035ed0ac0a4f6d2b2dece80220f775
Anirudh Mishra <anirudh5mar@gmail.com>
2022-12-31 13:59:54 +0530

    Created test for events

---
+++ pkg/post_manager/test_event_sink.go
@@ -0,0 +1,40 @@
+package post_manager
+
+import (
+	om "github.com/ani5msr/microservices-project/pkg/object_model"
+)
+
+type testEventsSink struct {
+	addPostEvents     map[string][]*om.Post
+	updatePostEvents  map[string][]*om.Post
+	deletedPostEvents map[string][]string
+}
+
+func (s *testEventsSink) OnPostAdded(username string, post *om.Post) {
+	if s.addPostEvents[username] == nil {
+		s.addPostEvents[username] = []*om.Post{}
+	}
+	s.addPostEvents[username] = append(s.addPostEvents[username], post)
+}
+
+func (s *testEventsSink) OnLinkUpdated(username string, link *om.Post) {
+	if s.updatePostEvents[username] == nil {
+		s.updatePostEvents[username] = []*om.Post{}
+	}
+	s.updatePostEvents[username] = append(s.updatePostEvents[username], link)
+}
+
+func (s *testEventsSink) OnLinkDeleted(username string, url string) {
+	if s.deletedPostEvents[username] == nil {
+		s.deletedPostEvents[username] = []string{}
+	}
+	s.deletedPostEvents[username] = append(s.deletedPostEvents[username], url)
+}
+
+func newPostManagerEventsSink() *testEventsSink {
+	return &testEventsSink{
+		map[string][]*om.Post{},
+		map[string][]*om.Post{},
+		map[string][]string{},
+	}
+}
